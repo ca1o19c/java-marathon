ae33245df8db51ca019fdee0a8f6b5027035d54c
Caio Antonio <caiocichetti08@gmail.com>
2021-10-07 22:37:32 -0300

    feat: created primitives type

---
+++ src/academy/javamarathon/introduction/PrimitivesType.java
@@ -0,0 +1,27 @@
+package academy.javamarathon.introduction;
+
+public class PrimitivesType {
+    public static void main(String[] args) {
+        int age = 19;
+        long largeNumber = 100000L;
+        double salaryDouble = 2000.0D;
+        float salaryFloat = 2500.0F;
+        byte ageByte = 10;
+        short ageShort = 10;
+        boolean logic = true;
+        char character = 'M';
+        String text = "This is a large text";
+        var name = "Caio";
+
+        System.out.println(age);
+        System.out.println(largeNumber);
+        System.out.println(salaryDouble);
+        System.out.println(salaryFloat);
+        System.out.println(ageByte);
+        System.out.println(ageShort);
+        System.out.println(logic);
+        System.out.println(character);
+        System.out.println(text);
+        System.out.println(name);
+    }
+}

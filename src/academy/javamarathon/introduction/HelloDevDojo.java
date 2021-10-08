cc7ec45b965cf6c8eca38597c6657872314c2e4c
Caio Antonio <caiocichetti08@gmail.com>
2021-10-07 21:47:35 -0300

    refactor: update name folder

---
+++ src/academy/javamarathon/introduction/HelloDevDojo.java
@@ -0,0 +1,13 @@
+package academy.javamarathon.introduction;
+
+/**
+ * Isso é um comentário javadoc
+ */
+
+
+public class HelloDevDojo {
+    public static void main(String[] args) {
+        // Isso é um comentário de uma linha
+        System.out.println("KA-ME-HA-ME-HA");
+    }
+}

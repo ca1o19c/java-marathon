5a410954e13f19d9218797cc63041737ca1adc1c
Caio Antonio <caiocichetti08@gmail.com>
2021-10-08 21:17:28 -0300

    feat: structure conditional

--- src/academy/javamarathon/introduction/StructureConditional03.java
+++ src/academy/javamarathon/introduction/StructureConditional03.java
@@ -5,7 +5,7 @@ public class StructureConditional03 {
         double salary = 6000;
         String donateMessage = "I will donate 600 to DevDojo";
         String notDonateMessage = "I will not donate to DevDojo";
-        String result = (salary > 5000) ? donateMessage : notDonateMessage;
+        String result = salary > 5000 ? donateMessage : notDonateMessage;
 
         System.out.println(result);
     }

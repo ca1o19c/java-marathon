095512af77c3884b5ac6284d29f217dbbb333e01
Caio Antonio <caiocichetti08@gmail.com>
2021-10-08 22:07:50 -0300

    feat: repeating structure

---
+++ src/academy/javamarathon/introduction/RepeatingStructure.java
@@ -0,0 +1,23 @@
+package academy.javamarathon.introduction;
+
+public class RepeatingStructure {
+    public static void main(String[] args) {
+        int count = 0;
+
+        while (count < 10) {
+            System.out.println("inside while -> " + count);
+            count++;
+        }
+
+        count = 0;
+
+        do {
+            System.out.println("inside do while -> " + count);
+            count++;
+        } while (count < 10);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("inside for -> " + i);
+        }
+    }
+}

7509cd72cb1d7ff961cfc63048856e3896388e57
Caio Antonio <caiocichetti08@gmail.com>
2021-10-13 19:00:58 -0300

    feat: arrays

--- src/academy/javamarathon/introduction/Arrays01.java
+++ src/academy/javamarathon/introduction/Arrays01.java
@@ -4,6 +4,10 @@ public class Arrays01 {
     public static void main(String[] args) {
         int[] ages = new int[3];
 
-        System.out.println();
+        ages[0] = 19;
+        ages[1] = 25;
+        ages[2] = 32;
+
+        System.out.println(ages[0]);
     }
 }

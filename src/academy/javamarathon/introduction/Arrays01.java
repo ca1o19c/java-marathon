351ee888671b6e3cedda8a8ee6992c24ecc5206b
Caio Antonio <caiocichetti08@gmail.com>
2021-10-13 18:58:26 -0300

    feat: arrays

---
+++ src/academy/javamarathon/introduction/Arrays01.java
@@ -0,0 +1,9 @@
+package academy.javamarathon.introduction;
+
+public class Arrays01 {
+    public static void main(String[] args) {
+        int[] ages = new int[3];
+
+        System.out.println();
+    }
+}

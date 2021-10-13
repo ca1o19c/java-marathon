2413cb1feaa3f2585fca8fd3155b1a60952e415f
Caio Antonio <caiocichetti08@gmail.com>
2021-10-13 19:16:21 -0300

    feat: arrays

---
+++ src/academy/javamarathon/introduction/Arrays02.java
@@ -0,0 +1,16 @@
+package academy.javamarathon.introduction;
+
+public class Arrays02 {
+    public static void main(String[] args) {
+        // byte, short, int, long, float and double -> 0
+        // char -> '\u0000'
+        // boolean -> false
+        // String null
+
+        String[] names = new String[3];
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+    }
+}

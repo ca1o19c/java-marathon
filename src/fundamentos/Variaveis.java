f2d185eb864e0c3faa032fdd856d1082e77a85ce
Caio Antonio <caiocichetti08@gmail.com>
2021-10-14 16:49:30 -0300

    Variaveis

---
+++ src/fundamentos/Variaveis.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        int idade = 19;
+        String nome = "Caio";
+        String nomeDoMeuCachorro = "Maggie";
+
+        System.out.println("Idade = " + idade);
+        System.out.println("Nome = " + nome);
+        System.out.println("Nome do cachorro = " + nomeDoMeuCachorro);
+    }
+}

8a00706b8dc7fbd4e128d3d5b70d94e1cf76b19b
Caio Antonio <caiocichetti08@gmail.com>
2021-10-16 20:13:32 -0300

    Exercício

---
+++ src/javacore/introducaoclasses/test/CarroTest.java
@@ -0,0 +1,23 @@
+package javacore.introducaoclasses.test;
+
+import javacore.introducaoclasses.dominio.Carro;
+
+public class CarroTest {
+    public static void main(String[] args) {
+        Carro fusca = new Carro();
+
+        fusca.nome = "Volkswagen Fusca";
+        fusca.modelo = "Typ 1";
+        fusca.ano = 1938;
+
+        System.out.println("nome: " + fusca.nome + " modelo: " + fusca.modelo + " ano: " + fusca.ano);
+
+        Carro mobi = new Carro();
+
+        mobi.nome = "Fiat Mobi";
+        mobi.modelo = "Like";
+        mobi.ano = 2022;
+
+        System.out.println("nome: " + mobi.nome + " modelo: " + mobi.modelo + " ano: " + mobi.ano);
+    }
+}

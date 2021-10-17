d56393aa9f84099f9160082fc26c42cba4617575
Caio Antonio <caiocichetti08@gmail.com>
2021-10-16 22:05:36 -0300

    Parâmetros tipo referência

--- src/javacore/introducaometodos/dominio/Calculadora.java
+++ src/javacore/introducaometodos/dominio/Calculadora.java
@@ -27,4 +27,13 @@ public class Calculadora {
 
         System.out.println(num1 / num2);
     }
+
+    public void alteraDoisNumeros(int numero1, int numero2) {
+        numero1 = 99;
+        numero2 = 33;
+
+        System.out.println("Dentro de 'alteraDoisNumeros'");
+        System.out.println("'numero1' " + numero1);
+        System.out.println("'numero2' " + numero2);
+    }
 }

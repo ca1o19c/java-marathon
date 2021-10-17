54d5470e452453cd3162d2c3565d206ffa6b29cd
Caio Antonio <caiocichetti08@gmail.com>
2021-10-17 15:15:21 -0300

    Varargs

--- src/javacore/introducaometodos/test/CalculadoraTest05.java
+++ src/javacore/introducaometodos/test/CalculadoraTest05.java
@@ -3,7 +3,7 @@ package javacore.introducaometodos.test;
 import javacore.introducaometodos.dominio.Calculadora;
 
 public class CalculadoraTest05 {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Calculadora calculadora = new Calculadora();
 
         int[] numeros = {1, 2, 3, 4, 5};

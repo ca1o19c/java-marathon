e5396d8513245df879f4b40bb698a2cb65875c64
Caio Antonio <caiocichetti08@gmail.com>
2021-10-16 21:23:49 -0300

    Retorno

--- src/javacore/introducaometodos/dominio/Calculadora.java
+++ src/javacore/introducaometodos/dominio/Calculadora.java
@@ -17,4 +17,14 @@ public class Calculadora {
     public double divideDoisNumeros(double num1, double num2) {
         return (num2 != 0) ? num1 / num2 : 0;
     }
+
+    public void imprimeDivisaoDeDoisNumeros(double num1, double num2) {
+
+        if (num2 == 0) {
+            System.out.println("Não existe divisão por zero");
+            return;
+        }
+
+        System.out.println(num1 / num2);
+    }
 }

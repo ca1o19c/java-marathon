e02b37aa10c32031ac19d34a0dbcf2cb30cf5042
Caio Antonio <caiocichetti08@gmail.com>
2021-10-16 22:06:45 -0300

    Parâmetros tipo referência

--- src/javacore/introducaometodos/test/EstudanteTest01.java
+++ src/javacore/introducaometodos/test/EstudanteTest01.java
@@ -18,9 +18,6 @@ public class EstudanteTest01 {
         estudante02.sexo = 'F';
 
         impressora.imprime(estudante01);
-
-        System.out.println("---------------------");
-
         impressora.imprime(estudante02);
     }
 }

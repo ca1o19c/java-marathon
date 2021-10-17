53f4b779d16e6c116cf8392c80c4e34ad3c0cf00
Caio Antonio <caiocichetti08@gmail.com>
2021-10-16 22:05:36 -0300

    Parâmetros tipo referência

---
+++ src/javacore/introducaometodos/test/CalculadoraTest04.java
@@ -0,0 +1,18 @@
+package javacore.introducaometodos.test;
+
+import javacore.introducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest04 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        int a = 1;
+        int b = 2;
+
+        calculadora.alteraDoisNumeros(a, b);
+
+        System.out.println("Dentro de 'CalculadoraTest04'");
+        System.out.println("'a' " + a);
+        System.out.println("'b' " + b);
+    }
+}
---
+++ src/javacore/introducaometodos/test/EstudanteTest01.java
@@ -0,0 +1,26 @@
+package javacore.introducaometodos.test;
+
+import javacore.introducaometodos.dominio.Estudante;
+import javacore.introducaometodos.dominio.ImpressoraEstudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        ImpressoraEstudante impressora = new ImpressoraEstudante();
+        Estudante estudante01 = new Estudante();
+        Estudante estudante02 = new Estudante();
+
+        estudante01.nome = "Caio";
+        estudante01.idade = 19;
+        estudante01.sexo = 'M';
+
+        estudante02.nome = "Gabi";
+        estudante02.idade = 19;
+        estudante02.sexo = 'F';
+
+        impressora.imprime(estudante01);
+
+        System.out.println("---------------------");
+
+        impressora.imprime(estudante02);
+    }
+}

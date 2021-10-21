3c4d822ba18ffeaf568fd77c06f0e67ca3732fa7
Caio Antonio <caio.antonio.c@outlook.com>
2021-10-21 20:32:59 -0300

    Blocos de inicialização

---
+++ src/javacore/blocosinicializacao/test/CervejaTest.java
@@ -0,0 +1,9 @@
+package javacore.blocosinicializacao.test;
+
+import javacore.blocosinicializacao.dominio.Cerveja;
+
+public class CervejaTest {
+    public static void main(String[] args) {
+        Cerveja cerveja = new Cerveja();
+    }
+}

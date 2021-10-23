ec6f75dcf05415fffef4d469deb3bcf90c4123eb
Caio Antonio <caio.antonio.c@outlook.com>
2021-10-22 22:35:16 -0300

    Arrays com Objetos

---
+++ src/javacore/associacao/dominio/Jogador.java
@@ -0,0 +1,24 @@
+package javacore.associacao.dominio;
+
+public class Jogador {
+    private String nome;
+
+    public Jogador(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Jogador{" +
+                "nome='" + nome + '\'' +
+                '}';
+    }
+}

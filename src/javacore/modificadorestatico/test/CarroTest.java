df771b6664390bdf2f1f3a48ecb32f61c638015e
Caio Antonio <caio.antonio.c@outlook.com>
2021-10-22 21:44:18 -0300

    Modificador static

---
+++ src/javacore/modificadorestatico/test/CarroTest.java
@@ -0,0 +1,19 @@
+package javacore.modificadorestatico.test;
+
+import javacore.modificadorestatico.dominio.Carro;
+
+public class CarroTest {
+    public static void main(String[] args) {
+        Carro c1 = new Carro("Fiat Mobi", 220);
+        Carro c2 = new Carro("BMW", 280);
+        Carro c3 = new Carro("Toro", 240);
+
+        System.out.println(Carro.velocidadeLimite);
+        Carro.velocidadeLimite = 180;
+        System.out.println(Carro.velocidadeLimite);
+
+        c1.imprime();
+        c2.imprime();
+        c3.imprime();
+    }
+}

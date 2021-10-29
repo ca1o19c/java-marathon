fee44f0e5d25d4ad3e5c5404531ffb0a344da5f3
Caio Antonio <caio.antonio.c@outlook.com>
2021-10-29 19:55:57 -0300

    Sobrescrita do método toString

---
+++ src/javacore/sobrescrita/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package javacore.sobrescrita.test;
+
+import javacore.sobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        var anime = new Anime("Dragon Ball");
+
+        System.out.println(anime);
+    }
+}

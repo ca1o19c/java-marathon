56bfb804b350fe1682c4bfccb70d69353516783d
Caio Antonio <caio.antonio.c@outlook.com>
2021-10-29 17:38:26 -0300

    Super

--- src/javacore/heranca/dominio/Funcionario.java
+++ src/javacore/heranca/dominio/Funcionario.java
@@ -1,9 +1,16 @@
 package javacore.heranca.dominio;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Funcionario extends Pessoa {
     private BigDecimal salario;
+
+    @Override
+    public void imprime() {
+        super.imprime();
+        System.out.println(this.salario);
+    }
 
     public BigDecimal getSalario() {
         return salario;
@@ -11,5 +18,25 @@ public class Funcionario extends Pessoa {
 
     public void setSalario(BigDecimal salario) {
         this.salario = salario;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Funcionario that = (Funcionario) o;
+        return Objects.equals(salario, that.salario);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(salario);
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "salario=" + salario +
+                '}';
     }
 }

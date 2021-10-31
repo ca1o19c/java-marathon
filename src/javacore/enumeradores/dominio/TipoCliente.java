f5ba71601bf4819f301744c7b36bd411be9746ac
Caio Antonio <caio.antonio.c@outlook.com>
2021-10-31 10:22:32 -0300

    [Enumeradores] Construtores e atributos

--- src/javacore/enumeradores/dominio/TipoCliente.java
+++ src/javacore/enumeradores/dominio/TipoCliente.java
@@ -1,6 +1,22 @@
 package javacore.enumeradores.dominio;
 
 public enum TipoCliente {
-    PESSOA_FISICA,
-    PESSOA_JURIDICA
+    PESSOA_FISICA(1, "Pessoa Física"),
+    PESSOA_JURIDICA(2, "Pessoa Jurídica");
+
+    private final int tipoInt;
+    private final String tipoString;
+
+    TipoCliente(int tipoInt, String tipoString) {
+        this.tipoInt = tipoInt;
+        this.tipoString = tipoString;
+    }
+
+    @Override
+    public String toString() {
+        return "TipoCliente{" +
+                "tipoInt=" + tipoInt +
+                ", tipoString='" + tipoString + '\'' +
+                '}';
+    }
 }
--- src/javacore/enumeradores/test/ClienteTest01.java
+++ src/javacore/enumeradores/test/ClienteTest01.java
@@ -2,13 +2,15 @@ package javacore.enumeradores.test;
 
 import javacore.enumeradores.dominio.Cliente;
 
+import static javacore.enumeradores.dominio.Cliente.TipoPagamento.CREDITO;
+import static javacore.enumeradores.dominio.Cliente.TipoPagamento.DEBITO;
 import static javacore.enumeradores.dominio.TipoCliente.PESSOA_FISICA;
 import static javacore.enumeradores.dominio.TipoCliente.PESSOA_JURIDICA;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
-        var cliente1 = new Cliente("Cliente1", PESSOA_FISICA);
-        var cliente2 = new Cliente("Cliente2", PESSOA_JURIDICA);
+        var cliente1 = new Cliente("Cliente1", PESSOA_FISICA, DEBITO);
+        var cliente2 = new Cliente("Cliente2", PESSOA_JURIDICA, CREDITO);
 
         System.out.println(cliente1);
         System.out.println(cliente2);

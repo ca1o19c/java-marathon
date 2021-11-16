849cd18bd5aa191f0e50426d8cf6767550b602db
Caio Roberto <caio.roberto@sensedia.com>
2021-11-16 10:11:49 -0300

    Adicionei o codigo das aulas de introducao

--- src/introducao/ArraysMultidimensionais01.java
+++ src/introducao/ArraysMultidimensionais01.java
@@ -4,17 +4,17 @@ public class ArraysMultidimensionais01 {
     public static void main(String[] args) {
         int[][] dias = new int[3][3];
 
-        dias[0][0] = 31;
-        dias[0][1] = 28;
-        dias[0][2] = 31;
+        dias[0][0] = 1;
+        dias[0][1] = 2;
+        dias[0][2] = 3;
 
-        dias[1][0] = 31;
-        dias[1][1] = 28;
-        dias[1][2] = 31;
+        dias[1][0] = 4;
+        dias[1][1] = 5;
+        dias[1][2] = 6;
 
-        dias[2][0] = 31;
-        dias[2][1] = 28;
-        dias[2][2] = 31;
+        dias[2][0] = 7;
+        dias[2][1] = 8;
+        dias[2][2] = 9;
 
         // Imprimindo no modo raiz
         System.out.print(dias[0][0] + " ");
@@ -37,7 +37,7 @@ public class ArraysMultidimensionais01 {
 
         // Imprimindo no modo nutella XD
         for (int i = 0; i < dias.length; i++) {
-            for (int j = 0; j < dias.length; j++) {
+            for (int j = 0; j < dias[i].length; j++) {
                 System.out.print(dias[i][j] + " ");
             }
             System.out.println("");

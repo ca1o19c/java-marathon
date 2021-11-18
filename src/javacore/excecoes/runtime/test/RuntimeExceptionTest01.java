0e7cf0388e6712c17f15a103542a8dbec7fcbf6a
Caio Antonio <caio.antonio.c@outlook.com>
2021-11-17 21:47:53 -0300

    RuntimeException

---
+++ src/javacore/excecoes/runtime/test/RuntimeExceptionTest01.java
@@ -0,0 +1,13 @@
+package javacore.excecoes.runtime.test;
+
+public class RuntimeExceptionTest01 {
+    public static void main(String[] args) {
+        // Checked e Unchecked
+
+        Object obj = null;
+        System.out.println(obj.toString());
+
+        int[] nums = {1, 2};
+        System.out.println(nums[2]);
+    }
+}

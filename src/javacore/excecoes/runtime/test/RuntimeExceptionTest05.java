9a83dd5156e4c63fa851546415bc548696f67383
Caio Roberto <caio.roberto@sensedia.com>
2021-11-23 10:15:13 -0300

    Multi catch em linha

---
+++ src/javacore/excecoes/runtime/test/RuntimeExceptionTest05.java
@@ -0,0 +1,32 @@
+package javacore.excecoes.runtime.test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class RuntimeExceptionTest05 {
+    public static void main(String[] args) {
+
+        try {
+            throw new ArithmeticException();
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException e) {
+            System.out.println("Dentro do \"ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException\"");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Dentro do \"IndexOutOfBoundsException\"");
+        } catch (RuntimeException e) {
+            System.out.println("Dentro do \"RuntimeException\"");
+        }
+
+        try {
+            talvezLanceException();
+        } catch (SQLException | FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void talvezLanceException() throws SQLException, FileNotFoundException, IOException {
+        // TODO document why this method is empty
+    }
+}

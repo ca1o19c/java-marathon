6e6cec6f238d8fd2f97b4d5919905ed396e4336a
Caio Roberto <caio.roberto@sensedia.com>
2021-12-23 14:43:41 -0300

    Strings

---
+++ src/javacore/strings/test/StringTest02.java
@@ -0,0 +1,22 @@
+package javacore.strings.test;
+
+public class StringTest02 {
+    public static void main(String[] args) {
+        String nome = "Maggie";
+        String nome2 = "     Maggie          ";
+        String numeros = "012345";
+
+        System.out.println(nome.charAt(0));
+        System.out.println(nome.length());
+        System.out.println(nome.replace("M", "L"));
+        System.out.println(nome.toLowerCase());
+        System.out.println(nome.toUpperCase());
+        System.out.println(nome2.trim());
+
+        System.out.println(numeros.length());
+        System.out.println(numeros.substring(0, 2));
+        System.out.println(numeros.substring(0, numeros.length()));
+        System.out.println(numeros.substring(3));
+        System.out.println(numeros.charAt(numeros.length() - 1));
+    }
+}

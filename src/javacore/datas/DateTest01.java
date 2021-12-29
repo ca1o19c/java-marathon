c10f266de3feb8f1f2af21102b8473a3bacbb1a3
Caio Roberto <caio.roberto@sensedia.com>
2021-12-29 15:15:43 -0300

    Date

---
+++ src/javacore/datas/DateTest01.java
@@ -0,0 +1,15 @@
+package javacore.datas;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+        Date data = new Date(1_000_000_000_000L); // Long milissegundos
+
+        System.out.println(data);
+
+        data.setTime(1_640_801_516_408L);
+
+        System.out.println(data);
+    }
+}

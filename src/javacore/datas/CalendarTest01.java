5d6fe88c004318c3d928875f5a29ecfd5a110457
Caio Roberto <caio.roberto@sensedia.com>
2022-01-03 08:56:17 -0300

    Calendar

---
+++ src/javacore/datas/CalendarTest01.java
@@ -0,0 +1,30 @@
+package javacore.datas;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class CalendarTest01 {
+    public static void main(String[] args) {
+        Calendar calendario = Calendar.getInstance();
+
+        Date data = calendario.getTime();
+
+        System.out.println(data);
+
+        if (calendario.getFirstDayOfWeek() == Calendar.SUNDAY) {
+            System.out.println("Domingão é o primeiro dia da semana");
+        }
+
+        System.out.println(calendario.get(Calendar.DAY_OF_WEEK));
+        System.out.println(calendario.get(Calendar.DAY_OF_MONTH));
+        System.out.println(calendario.get(Calendar.DAY_OF_YEAR));
+        System.out.println(calendario.get(Calendar.DAY_OF_WEEK_IN_MONTH));
+
+        calendario.add(Calendar.DAY_OF_MONTH, 3);
+        calendario.add(Calendar.HOUR, 6);
+
+        data = calendario.getTime();
+
+        System.out.println(data);
+    }
+}
